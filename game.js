996077e5b6a70ca38939a4ac2b7559bff1adce67
Isaiah Rice <83632218+Isaiah-R@users.noreply.github.com>
2021-05-28 11:13:03 -0700

    Update game.js

--- game.js
+++ game.js
@@ -9,7 +9,7 @@ const gameBoard = document.getElementById('game-board')
 function main(currentTime) {
   if (gameOver) {
     if (confirm('You lost. Press Ok to restart.')) {
-      window.location = '/'
+      window.location = '/snakeIndex.html'
     }
     return
   }
